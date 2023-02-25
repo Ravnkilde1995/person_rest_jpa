64b60ded242b6a2c11da4c8f315448250f4f1073
ravnkilde1995 <ravnkilde1995@gmail.com>
2023-02-25 21:03:39 +0100

    added PersonNotFoundException

--- src/main/java/facades/IPersonFacade.java
+++ src/main/java/facades/IPersonFacade.java
@@ -2,16 +2,17 @@ package facades;
 
 import dtos.PersonDTO;
 import dtos.PersonsDTO;
+import errorhandling.PersonNotFoundException;
 
 public interface IPersonFacade {
     public PersonDTO addPerson(String fName, String lName, String phone);
 
-    public PersonDTO deletePerson(long id);
+    public PersonDTO deletePerson(long id) throws PersonNotFoundException;
 
-    public PersonDTO getPerson(int id);
+    public PersonDTO getPerson(int id)  throws PersonNotFoundException;
 
     public PersonsDTO getAllPersons();
 
-    public PersonDTO editPerson(PersonDTO p);
+    public PersonDTO editPerson(PersonDTO p) throws PersonNotFoundException;
 
 }
